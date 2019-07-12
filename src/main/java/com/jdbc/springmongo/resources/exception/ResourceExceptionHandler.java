4affe393df870e4d72843384f451a629cff36b3d
Julio Baltazar <julio.baltazar@ifactory.com.br>
2019-07-12 10:10:08 -0300

    obtendo usuario pelo id

---
+++ src/main/java/com/jdbc/springmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.jdbc.springmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jdbc.springmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest req) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), req.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
