93319cf2357c83ebfa1768efca6be87785c8896e
Julio Baltazar <julio.baltazar@ifactory.com.br>
2019-07-23 09:36:50 -0300

    obtendo um post por id

---
+++ src/main/java/com/jdbc/springmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.jdbc.springmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jdbc.springmongo.domain.Post;
+import com.jdbc.springmongo.repositories.PostRepository;
+import com.jdbc.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
